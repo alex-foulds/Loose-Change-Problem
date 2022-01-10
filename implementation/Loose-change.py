6e7fbdb4dc12260e04618af995be9a2f23cb1d8f
Alex Foulds <alex.foulds333@gmail.com>
2022-01-10 22:01:38 +0000

    New implementation flow requires additional rounding for 0.01 check

--- implementation/Loose-change.py
+++ implementation/Loose-change.py
@@ -5,7 +5,7 @@ def main():
     coins = [2,1,0.5,0.2,0.1,0.05,0.02,0.01];
     denomination = ["£2","£1","50p","20p","10p","5p","2p","1p"];
     finalDict = {};
-    change = 0.98;
+    change = 4.98;
 
     calculateChangeIterate(coins,denomination,finalDict,change);
 
@@ -41,15 +41,16 @@ def calculateChangeDivision(change):
     
     if change>0:
         #Approach uses the remaining values after floored division to work out the next coin values
+        #Round has had to be used to avoid errors when 0.009999 would not add 1 to '1p'
         finalDict["£2"] = int(change//2);
-        finalDict["£1"] = int(change % 2 // 1);
-        finalDict["50p"]= int(change % 2 % 1 // 0.5);
-        finalDict["20p"]= int(change % 2 % 1 % 0.5 // 0.2);
-        finalDict["10p"]= int(change % 2 % 1 % 0.5 % 0.2 // 0.1);
-        finalDict["5p"] = int(change % 2 % 1 % 0.5 % 0.2 % 0.1 // 0.05);
-        finalDict["2p"] = int(change % 2 % 1 % 0.5 % 0.2 % 0.1 % 0.05 // 0.02);
-        #Round to account for times where 0.009 would return 1p:'0'error
-        finalDict["1p"] = int(round(change % 2 % 1 % 0.5 % 0.2 % 0.1 % 0.05 % 0.02,2)//0.01);
+        finalDict["£1"] = int(round(change % 2,2) // 1);
+        finalDict["50p"]= int(round(change % 2 % 1,2) // 0.5);
+        finalDict["20p"]= int(round(change % 2 % 1 % 0.5,2) // 0.2);
+        finalDict["10p"]= int(round(change % 2 % 1 % 0.5 % 0.2,2) // 0.1);
+        finalDict["5p"] = int(round(change % 2 % 1 % 0.5 % 0.2 % 0.1,2) // 0.05);
+        finalDict["2p"] = int(round(change % 2 % 1 % 0.5 % 0.2 % 0.1 % 0.05,2) // 0.02);
+        #Python maths results in strange error where 2p and 1p are both 2 for 0.99
+        finalDict["1p"] = int(round(round(change % 2 % 1 % 0.5 % 0.2 % 0.1 % 0.05,2) % 0.02,2) //0.01);
         print(finalDict);
     else:
         print(finalDict);
